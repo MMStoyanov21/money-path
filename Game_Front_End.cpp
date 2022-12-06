830de3f7f050bc06a6bd45336195270b887bfe33
TDTanev21 <TDTanev21@codingburgas.bg>
2022-12-06 21:28:03 +0200

    Bigger map + functions for true/wrong answer and beggining

--- Game_Front_End.cpp
+++ Game_Front_End.cpp
@@ -3,25 +3,94 @@
 #include <Windows.h>
 
 using namespace std;
-int main()
 
+int counter = 3;
+int player = 107;
+
+void FrontEnd()
 {
-	cout << setw(45) << " ________________________ " << endl;
-	cout << setw(42) << "|	                    |" << "                         You have 3 lives left" << endl;
-	cout << setw(45) << "|  Vapros za 100hil leva |" << endl;
-	cout << setw(45) << "|___________  ___________|" << endl;
+	system("cls");
+	cout << setw(52) << " ______________________________________ " << endl;
+	cout << setw(44) << "    |	    Question for 100 leva	   |" << "					 You have " << counter << " lives left" << endl;
+	cout << setw(24) << "   |  					   |" << endl;
+	cout << setw(44) << "|	     Which is the biggest	   |" << endl;
+	cout << setw(36) << "|		 country in the		   |" << endl;
+	cout << setw(32) << "   |		     world?		   |" << endl;
+	cout << setw(24) << "   |  					   |" << endl;
+	cout << setw(52) << "|__________________  __________________|" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
 	cout << setw(33) << "||" << endl;
 	cout << setw(33) << "||" << endl;
 	cout << "-------------------------------||---------------------------------------------------------------------------------------";
-	cout << "                                                                                                          " << endl;
-	cout << "                                                                                                           " << endl;
-	cout << setw(107) << "[] " << endl;
+	cout << endl;
+	cout << endl;
+	cout << setw(player) << "[] " << endl;
 	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
 	cout << endl;
 	cout << endl;
 	cout << endl;
 	cout << "------------------------------------------------------------------------------------------------------------------------";
 
+}
+void TrueAnswer()
+{
+	system("cls");
 
+	cout << setw(52) << " ______________________________________ " << endl;
+	cout << setw(13) << "|" << setw(29) << "Question for 100 leva" << setw(10) << "|" << "					 You have " << counter << " lives left" << endl;
+	cout << setw(13) << "|" << setw(39) << "|" << endl;
+	cout << setw(13) << "|" << setw(29) << "Which is the biggest" << setw(10) << "|" << endl;
+	cout << setw(13) << "|" << setw(25) << "country in the" << setw(14) << "|" << endl;
+	cout << setw(13) << "|" << setw(23) << "world ?" << setw(16) << "|" << endl;
+	cout << setw(13) << "|" << setw(39) << "|" << endl;
+	cout << setw(52) << "|__________________  __________________|" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << "-------------------------------||---------------------------------------------------------------------------------------";
+	cout << "                                                                                                          " << endl;
+	cout << "                                                                                                           " << endl;
+	cout << setw(player - 10) << "[] " << endl;
+	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "------------------------------------------------------------------------------------------------------------------------";
+
+}
+void WrongAnswer()
+{
+	counter--;
+	system("cls");
+	cout << setw(52) << " ______________________________________ " << endl;
+	cout << setw(44) << "    |	    Question for 100 leva	   |" << "					 You have " << counter << " lives left" << endl;
+	cout << setw(24) << "   |  					   |" << endl;
+	cout << setw(44) << "|	     Which is the biggest	   |" << endl;
+	cout << setw(36) << "|		 country in the		   |" << endl;
+	cout << setw(32) << "   |		     world?		   |" << endl;
+	cout << setw(24) << "   |  					   |" << endl;
+	cout << setw(52) << "|__________________  __________________|" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << "-------------------------------||---------------------------------------------------------------------------------------";
+	cout << endl;
+	cout << endl;
+	cout << setw(player) << "[] " << endl;
+	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "------------------------------------------------------------------------------------------------------------------------";
+}
+int main()
+
+{
 
 }
