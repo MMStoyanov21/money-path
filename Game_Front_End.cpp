a049995cd89c6ba76c6aada6337888b57b7b763a
TDTanev21 <TDTanev21@codingburgas.bg>
2022-12-06 21:15:48 +0200

    The game's basic look

---
+++ Game_Front_End.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+int main()
+
+{
+	cout << setw(45) << " ________________________ " << endl;
+	cout << setw(42) << "|	                    |" << "                         You have 3 lives left" << endl;
+	cout << setw(45) << "|  Vapros za 100hil leva |" << endl;
+	cout << setw(45) << "|___________  ___________|" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << setw(33) << "||" << endl;
+	cout << "-------------------------------||---------------------------------------------------------------------------------------";
+	cout << "                                                                                                          " << endl;
+	cout << "                                                                                                           " << endl;
+	cout << setw(107) << "[] " << endl;
+	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << "------------------------------------------------------------------------------------------------------------------------";
+
+
+
+}
