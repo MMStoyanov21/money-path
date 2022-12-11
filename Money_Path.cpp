6aa49c6825c6313a1ed9482245f488563079f231
TDTanev21 <TDTanev21@codingburgas.bg>
2022-12-11 22:11:25 +0200

    Fixed big mistake in the question answering system

--- Money_Path.cpp
+++ Money_Path.cpp
@@ -7,25 +7,18 @@
 using namespace std;
 
 int counter = 3;
-int player = 107;
+int player = 7;
 int moneyCollected = 0, money = 100;
 string questions[8] = { "Which is the biggest country in the world?", "Which is the highest peak on the Balkan Peninsula?", "Which is the highest mountain in the world?","Which country's national football team won the 2018 WC in Russia?", "Which is the closest planet to the Sun?","Where is Lionel Messi from?","Which year Bulgaria was freed from Turkish slavery?","Which is the capital city of Canada?" };
-
+string currentQuestion;
 
 void Questions()
 {
+	
 	random_device(rd);
 	uniform_int_distribution<int> dist(0, 7);
-	cout << questions[dist(rd)];
-	if (questions[dist(rd)] == "")
-	{
-		cout << questions[dist(rd)];
-		if (questions[dist(rd)] == "")
-		{
-			cout << questions[dist(rd)];
+	currentQuestion = questions[dist(rd)];
 
-		}
-	}
 
 }
 
@@ -35,6 +28,7 @@ void FrontEnd()
 
 	cout << "Question: ";
 	Questions();
+	cout << currentQuestion;
 	cout << endl;
 	cout << endl;
 	cout << endl;
@@ -46,15 +40,17 @@ void FrontEnd()
 	cout << setw(45) << "|___________  ___________|" << endl;
 	cout << setw(33) << "||" << endl;
 	cout << setw(33) << "||" << endl;
-	cout << "-------------------------------||---------------------------------------------------------------------------------------";
-	cout << "  FINISH                                                                                                        " << endl;
+	cout << "-------------------------------||------------------------------------------------------------------------------------------------------------------------------";
+	cout <<setw(171)<<"FINISH" << endl;
 	cout << "                                                                                                           " << endl;
 	cout << setw(player) << "[] " << endl;
-	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
+	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
 	cout << endl;
 	cout << endl;
 	cout << endl;
-	cout << "------------------------------------------------------------------------------------------------------------------------";
+	cout << "---------------------------------------------------------------------------------------------------------------------------------------------------------------";
+
+
 
 
 
@@ -63,11 +59,13 @@ void TrueAnswer()
 {
 	money += 100;
 	moneyCollected += 100;
+
 	system("cls");
 	if (money <= 999)
 	{
 		cout << "Question: ";
 		Questions();
+		cout << currentQuestion;
 		cout << endl;
 		cout << endl;
 		cout << endl;
@@ -79,42 +77,43 @@ void TrueAnswer()
 		cout << setw(45) << "|___________  ___________|" << endl;
 		cout << setw(33) << "||" << endl;
 		cout << setw(33) << "||" << endl;
-		cout << "-------------------------------||---------------------------------------------------------------------------------------";
-		cout << "   FINISH                                                                                                          " << endl;
+		cout << "-------------------------------||-------------------------------------------------------------------------------------------------------------------------------";
+		cout << setw(171) << "FINISH" << endl;
 		cout << "                                                                                                           " << endl;
-		cout << setw(player -= 10) << "[] " << endl;
-		cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
+		cout << setw(player +=10) << "[] " << endl;
+		cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
 		cout << endl;
 		cout << endl;
 		cout << endl;
-		cout << "------------------------------------------------------------------------------------------------------------------------";
+		cout << "----------------------------------------------------------------------------------------------------------------------------------------------------------------"<<endl;
+
 
 	}
 	else
 	{
 		cout << "Question: ";
 		Questions();
+		cout << currentQuestion;
 		cout << endl;
 		cout << endl;
 		cout << endl;
-		cout << setw(44) << " _________________________" << endl;
-		cout << setw(19) << "|" << setw(26) << " |" << endl;
-		cout << setw(19) << "|" << setw(26) << " |" << "                         You have: " << counter << " lives left " << endl;
-		cout << setw(34) << "|  Question for " << money << " BGN  |" << "			    You have: " << moneyCollected << " BGN collected" << endl;
-		cout << setw(19) << "|" << setw(26) << " |" << endl;
-		cout << setw(45) << "|___________  ____________|" << endl;
+		cout << setw(45) << " ________________________ " << endl;
+		cout << setw(20) << "|" << setw(25) << "|" << endl;
+		cout << setw(20) << "|" << setw(25) << "|" << "                         You have: " << counter << " lives left " << endl;
+		cout << setw(35) << "|  Question for " << money << " BGN  |" << "			    You have: " << moneyCollected << " BGN collected" << endl;
+		cout << setw(20) << "|" << setw(25) << "|" << endl;
+		cout << setw(45) << "|___________  ___________|" << endl;
 		cout << setw(33) << "||" << endl;
 		cout << setw(33) << "||" << endl;
-		cout << "-------------------------------||---------------------------------------------------------------------------------------";
-		cout << "   FINISH                                                                                                          " << endl;
+		cout << "-------------------------------||-------------------------------------------------------------------------------------------------------------------------------";
+		cout << setw(171) << "FINISH" << endl;
 		cout << "                                                                                                           " << endl;
-		cout << setw(player -= 10) << "[] " << endl;
-		cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
+		cout << setw(player += 10) << "[] " << endl;
+		cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
 		cout << endl;
 		cout << endl;
 		cout << endl;
-		cout << "------------------------------------------------------------------------------------------------------------------------";
-
+		cout << "----------------------------------------------------------------------------------------------------------------------------------------------------------------"<<endl;
 	}
 
 
@@ -126,26 +125,27 @@ void WrongAnswer()
 
 	cout << "Question: ";
 	Questions();
+	cout << currentQuestion;
 	cout << endl;
 	cout << endl;
 	cout << endl;
 	cout << setw(45) << " ________________________ " << endl;
-	cout << setw(20) << "|" << setw(25) << "|" << endl;
-	cout << setw(20) << "|" << setw(25) << "|" << "                         You have: " << counter << " lives left " << endl;
-	cout << setw(35) << "|  Question for " << money << " BGN  |" << "			    You have: " << moneyCollected << " BGN collected" << endl;
-	cout << setw(20) << "|" << setw(25) << "|" << endl;
+	cout << setw(20) << "|" << setw(25) << " |" << endl;
+	cout << setw(20) << "|" << setw(25) << " |" << "                         You have: " << counter << " lives left " << endl;
+	cout << setw(35) << "|  Question for " << money << " BGN   |" << "			    You have: " << moneyCollected << " BGN collected" << endl;
+	cout << setw(20) << "|" << setw(25) << " |" << endl;
 	cout << setw(45) << "|___________  ___________|" << endl;
 	cout << setw(33) << "||" << endl;
 	cout << setw(33) << "||" << endl;
-	cout << "-------------------------------||---------------------------------------------------------------------------------------";
-	cout << "   FINISH                                                                                                      " << endl;
+	cout << "-------------------------------||-------------------------------------------------------------------------------------------------------------------------------";
+	cout << setw(171) << "FINISH" << endl;
 	cout << "                                                                                                           " << endl;
 	cout << setw(player) << "[] " << endl;
-	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
+	cout << "   =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====     =====";
 	cout << endl;
 	cout << endl;
 	cout << endl;
-	cout << "------------------------------------------------------------------------------------------------------------------------";
+	cout << "----------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
 
 
 }
@@ -177,6 +177,10 @@ void GameOver()
 	{
 		cout << "						   You won 500 BGN";
 	}
+	else if (moneyCollected >= 1000 && money <= 1499)
+	{
+		cout << "						  You won 1000 BGN";
+	}
 
 }
 void GameWon()
@@ -185,7 +189,7 @@ void GameWon()
 	cout << "					===============================" << endl;
 	cout << "					====CONGRATULATIONS YOU WON====" << endl;
 	cout << "					===============================" << endl;
-	cout << "						You won 1000 BGN";
+	cout << "						You won 1500 BGN";
 }
 
 int main()
@@ -203,10 +207,17 @@ int main()
 	cout << endl;
 
 
-	do {
+	while (true) {
 		cout << "Input your answer here:";
 		cin >> answer;
-		if (questions[0] == "Which is the biggest country in the world?" && answer == "Russia" || questions[1] == "Which is the highest peak on the Balkan Peninsula?" && answer == "Musalla" || questions[2] == "Which is the highest mountain in the world?" && answer == "Himalayas" || questions[3] == "Which country's national football team won the 2018 WC in Russia?" && answer == "France" || questions[4] == "Which is the closest planet to the Sun?" && answer == "Mercury" || questions[5] == "Where is Lionel Messi from?" && answer == "Argentina" || questions[6] == "Which year Bulgaria was freed from Turkish slavery?" && answer == "1878" || questions[7] == "Which is the capital city of Canada?" && answer == "Ottawa")
+		if (questions[0] == currentQuestion && answer == "Russia" ||
+			questions[1] == currentQuestion && answer == "Musalla" ||
+			questions[2] == currentQuestion && answer == "Himalayas" ||
+			questions[3] == currentQuestion && answer == "France" ||
+			questions[4] == currentQuestion && answer == "Mercury" ||
+			questions[5] == currentQuestion && answer == "Argentina" ||
+			questions[6] == currentQuestion && answer == "1878" ||
+			questions[7] == currentQuestion && answer == "Ottawa")
 		{
 			TrueAnswer();
 		}
@@ -219,12 +230,13 @@ int main()
 			GameOver();
 			break;
 		}
-		if (player <= 7)
+		if (player > 155)
 		{
 			GameWon();
 			break;
 		}
-	} while (true);
+		
+	}
 
 
 
